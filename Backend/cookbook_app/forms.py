9e8ea74a2f902d8defef132e5b8b7deb841f1d78
MartinPl0 <mathas000@gmail.com>
2023-05-23 15:34:42 +0200

    almost done

--- Backend/cookbook_app/forms.py
+++ Backend/cookbook_app/forms.py
@@ -3,6 +3,20 @@ from .models import Recipe, IngredientAmount, RecipeIngredient, MeasurementUnit
 from django_select2 import forms as s2forms
 from .models import Ingredient, Category
 from django.forms.widgets import ClearableFileInput
+from django.core.exceptions import ValidationError
+from django.core.files.images import get_image_dimensions
+
+def validate_image(image):
+    file_size = image.size
+    width, height = get_image_dimensions(image)
+    max_size = 1024 * 1024  # 1MB
+    max_width = 800  # pixels
+    max_height = 800  # pixels
+
+    if width > max_width or height > max_height:
+        raise ValidationError("Max size of an image is %s x %s" % (max_width, max_height))
+    if file_size > max_size:
+        raise ValidationError("Max file size is: %sKB" % str(max_size // 1024))
 
 class CustomClearableFileInput(ClearableFileInput):
     def get_template_substitution_values(self, value):
@@ -15,19 +29,21 @@ class CustomClearableFileInput(ClearableFileInput):
             'template_name': self.template_name,
         }
 
+
 class RecipeForm(forms.ModelForm):
     categories = forms.ModelMultipleChoiceField(
         queryset=Category.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
-    image = forms.ImageField(widget=CustomClearableFileInput)
+    image = forms.ImageField(widget=CustomClearableFileInput, validators=[validate_image])
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'wide-textarea'}))
-
 
     class Meta:
         model = Recipe
         fields = ('name', 'description', 'preparation_time', 'image', 'spiciness', 'steps', 'categories')
+
+
 class IngredientAmountForm(forms.ModelForm):
     unit = forms.ModelChoiceField(queryset=MeasurementUnit.objects.all(), widget=forms.Select(attrs={'class': 'unit-select'}))
 
